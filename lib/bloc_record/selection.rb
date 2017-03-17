0679a106358d75a075935eb89122873cf9620b33
Sam Dieck <samuel.dieck@gmail.com>
2017-03-17 15:38:12 -0700

    Finished assignment 4

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -89,6 +89,14 @@ module Selection
     if args.count > 1
       expression = args.shift
       params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
     end
 
     sql = <<-SQL
@@ -97,6 +105,64 @@ module Selection
     SQL
 
     rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+    orders = []
+
+    if args.count < 1
+      orders = args.first.to_s
+    else
+      args.each do |arg|
+        case arg
+        when String
+          orders << ascend_descend(arg)
+        when Hash
+          orders << ascend_descend(arg.map { |key,value| "#{key} #{value}" }.join(''))
+        when Symbol
+          orders << ascend_descend(arg)
+        end
+      end
+    end
+
+    orders.join(',')
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{orders}
+    SQL
+    rows_to_array(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+        SQL
+      when Hash
+        key = args.first.keys[0]
+        value = args.first.values[0]
+        rows = connection <<-SQL
+          SELECT * FROM
+          INNER JOIN #{key} ON #{key}.#{table}_id = #{table}.id
+          INNER JOIN #{value} ON #{value}.#{key}_id = #{key}.id
+        SQL
+      end
+    end
+
     rows_to_array(rows)
   end
 
@@ -110,5 +176,13 @@ module Selection
 
   def rows_to_array(rows)
     rows.map { |row| new(Hash[columns.zip(row)]) }
+  end
+
+  def ascend_descend(string)
+    if string.include?(" asc") || string.include?(" ASC") || string.include?(" desc") || string.include?(" DESC")
+      string
+    else
+      string << " ASC"
+    end
   end
 end
