830cd841637e9ac11095a32ad055e9cdcddf477a
Sam Dieck <samuel.dieck@gmail.com>
2017-04-10 17:09:36 -0700

    Assignment corrections

--- lib/bloc_record/connection.rb
+++ lib/bloc_record/connection.rb
@@ -5,7 +5,7 @@ module Connection
     if BlocRecord.database == :sqlite3
       @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
     elsif BlocRecord.database == :pg
-      @connection ||= Postgres::Database.new(BlocRecord.database_filename)
+      @connection ||= PG.connect(dbname: BlocRecord.database_filename)
     end
   end
 end
